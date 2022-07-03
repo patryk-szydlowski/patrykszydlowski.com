a107ce0420b81e53bbff961d31e43ec92f6e879d
Patryk Szydłowski <patrykszydlowski99@gmail.com>
2022-07-03 15:17:01 +0200

    chore: add tailwind support

--- src/root.tsx
+++ src/root.tsx
@@ -1,11 +1,15 @@
-import { type MetaFunction } from "@remix-run/cloudflare";
+import { type LinksFunction, type MetaFunction } from "@remix-run/cloudflare";
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+
+import tailwindStyles from "./styles/tailwind.generated.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Patryk Szydłowski",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links: LinksFunction = () => [{ href: tailwindStyles, rel: "stylesheet" }];
 
 export default () => (
   <html lang="en">
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,7 @@
-export default () => <div>Hello world</div>;
+export default () => (
+  <div className="hover:bg-amber-800">
+    <h1 className="-bottom-10 text-3xl font-bold underline hover:-bottom-1/3 hover:accent-red-500 ">
+      Hello world!
+    </h1>
+  </div>
+);
